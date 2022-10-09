465b968642d74a9f4d096c5058e9373a6609971b
75Engel <106105557+75Engel@users.noreply.github.com>
2022-10-09 20:00:49 +0200

    actualizacion

--- README.md
+++ README.md
@@ -1,1 +1,12 @@
 # proyecto_twitter
+
+Ejercicio final donde se hace un repaso a diversos aspectos que debe de llevarse a cabo en un trabajo de Data Scientist.
+
+El proyecto empieza con la **obtención de datos** a través de la API de *Twitter* y un proceso de ETL para llevar la información a un entorno SQL.
+
+Posteriormente se realiza un **Estudio Exploratorio de Datos (EDA)** sobre los mismos tanto por queries de SQL como con la librería pandas de python.
+
+Tras esto, se utiliza un modelo entrenado de **Natural Lenguage Processing** para estimar si los diferentes tweets obtenidos dan una respuesta positiva o negativa en relación a la pregunta: ¿El mensaje ha causado impacto en los usuarios?
+
+Finalmente se lleva a **implantación en internet** de la información obtenida en el punto 3 para el uso de los posibles usuarios/clientes.
+
---
+++ app.py
@@ -0,0 +1,50 @@
+from flask import Flask, request, jsonify
+
+import pickle
+#from sklearn.model_selection import cross_val_score
+import pandas as pd
+
+#import sqlite3
+#from datetime import datetime
+
+
+
+app = Flask(__name__)
+app.config['DEBUG'] = True
+
+@app.route("/", methods=['GET'])
+def hello():
+    return  "<h1>Bienvenido a la página principal del servicio ofrecido por Nomedalavidaenello</h1>\
+            <p><h5>web realizada por Javier Tenorio</h5></p>\
+            <p><h2>Servicio generado para estudiar si una campaña en Twitter será existosa o no</h2></p>"
+#            <p><h3>Introduzca aqui el Tweet a estudiar:</h3></p"
+            
+
+# --PREDICCIÓN--
+@app.route('/predict', methods=['GET','POST'])
+def predict():
+    model = pickle.load(open('api\model\sentiment_model','rb'))
+    
+    text = request.args.get('text', None)
+    file={'text':text}
+    df_test=pd.DataFrame()
+    df_test['text']=file
+
+    
+    if text is None:
+        return "Faltan argumentos para realizar la predicción"
+
+    prediction = model.predict(df_test)
+
+    if prediction == 0:
+        print('El TEXTO no genera impacto.')
+    else:
+        print('Ha conseguido un  TEXTO exitoso, le recomendamos que lo publique')
+
+
+
+
+
+
+
+#app.run()
